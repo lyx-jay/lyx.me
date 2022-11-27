df32dd8f9782be83c01e7ec6d9a8d2ccf5396a28
lvyuanxin <1498781418@qq.com>
2022-11-27 16:27:17 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Navbar from './pages/Nav/Navbar.vue'
+</script>
+
+<template>
+  <Navbar />
+
+</template>
+
+<style scoped lang="less">
+
+</style>
---
+++ src/pages/Nav/Navbar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>post</div>
+  <div>projects</div>
+  <div>photos</div>
+  <div></div>
+</template>
+
+<style scoped>
+
+</style>
